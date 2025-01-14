8cfc20b832d61eb9865be1074e405ed5c592755f
Raphaelkim96 <79755145+Raphaelkim96@users.noreply.github.com>
2025-01-14 16:32:06 +0900

    헤더파일

---
+++ Inc/delay_us.h
@@ -0,0 +1,17 @@
+/*
+ * delay_us.h
+ *
+ *  Created on: Jan 14, 2025
+ *      Author: gyromind
+ */
+
+#ifndef INC_DELAY_US_H_
+#define INC_DELAY_US_H_
+
+#include "main.h"
+#include "tim.h"
+
+void delay_us(uint16_t us);
+
+
+#endif /* INC_DELAY_US_H_ */
---
+++ Inc/stepper.h
@@ -0,0 +1,48 @@
+/*
+ * stepper.h
+ *
+ *  Created on: Jan 14, 2025
+ *      Author: gyromind
+ */
+
+#ifndef INC_STEPPER_H_
+#define INC_STEPPER_H_
+
+#include "delay_us.h"
+
+#define STEP_PER_RESOLUTION 4096
+#define DIR_CW              0
+#define DIR_CCW             1
+
+#define IN1_PIN             GPIO_PIN_1
+#define IN1_PORT            GPIOB
+#define IN2_PIN             GPIO_PIN_15
+#define IN2_PORT            GPIOB
+#define IN3_PIN             GPIO_PIN_14
+#define IN3_PORT            GPIOB
+#define IN4_PIN             GPIO_PIN_13
+#define IN4_PORT            GPIOB
+
+static const uint8_t HALF_STEP_SEQ[8][4] =
+    {
+        {1, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 1},
+        {0, 0, 0, 1},
+        {1, 0, 0, 1},
+    };
+
+
+void stepMotor(uint8_t step);
+
+void rotateSteps(uint16_t steps, uint8_t direction);
+void rotateDegree(uint16_t degrees, uint8_t direction);
+
+
+
+
+
+#endif /* INC_STEPPER_H_ */
